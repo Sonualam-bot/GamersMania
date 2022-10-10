5a69e43788e69290a16d0143f7951dc90f686b9d
Sonu <sonualam1599@gmail.com>
2022-10-11 00:00:42 +0530

    update : completed simple card using css
    made a character card using html and css

---
+++ script.js
@@ -0,0 +1,9 @@
+const userCardTemplate = document.querySelector("[data-user-template]");
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => {
+        const card = userCardTemplate.textContent.cloneNode(true).children[0];
+        console.log(card);
+    })
